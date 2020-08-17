30a836d03b2f7f3ff112eceeed90606f3237f76e
Arkadiusz Noster <arkadiusz.noster@gmail.com>
2020-08-17 16:29:35 +0200

    refactor

--- pkg/party/visitor-lister.go
+++ pkg/party/visitor-lister.go
@@ -1,13 +1,5 @@
 package party
 
-import "fmt"
-
-var _ fmt.Stringer = Visitor{}
-
-func (v Visitor) String() string {
-	return fmt.Sprintf("%s %s", v.Name, v.Surname)
-}
-
 type VisitorGroup string
 
 const (
--- pkg/party/visitor.go
+++ pkg/party/visitor.go
@@ -1,6 +1,14 @@
 package party
 
+import "fmt"
+
 type Visitor struct {
 	Name    string
 	Surname string
+}
+
+var _ fmt.Stringer = Visitor{}
+
+func (v Visitor) String() string {
+	return fmt.Sprintf("%s %s", v.Name, v.Surname)
 }
